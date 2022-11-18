c390079ee46c1718760ce6d4c1d808a96aa121a7
Jon Jahren <jon.jahren@gmail.com>
2022-11-18 17:38:12 +0100

    Added interrupt wip

--- src/Tasks/ultrasoncinterrupts.adb
+++ src/Tasks/ultrasoncinterrupts.adb
@@ -1,5 +1,17 @@
+-- Attempt at getting a working interrupt based ultrasonic readout
+
+with MicroBit.TimeHighspeed; use MicroBit.TimeHighspeed;
+with Ada.Interrupts; use Ada.Interrupts;
+with MicroBit.IOsForTasking; use MicroBit.IOsForTasking;
+with NRF_SVD.GPIO; use NRF_SVD.GPIO;
+use MicroBit;
+with HAL; use HAL;
+
 package body ultrasoncinterrupts is
-
+   protected ultrainterrupts is
+      procedure frontinterrupt
+      pragma Interrupt_Handler(ultrainterrupts);
    
-
+      begin
+         Attach_Handler(ultrainterrupts.frontinterrupt'Access, 
 end ultrasoncinterrupts;
