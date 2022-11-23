9379ac8bb89a22f39238f5d4ab3b227015475b86
Jon Jahren <jon.jahren@gmail.com>
2022-11-23 12:12:30 +0100

    Removed superflous comments, removed giveUp logic

--- src/Tasks/taskthink.adb
+++ src/Tasks/taskthink.adb
@@ -1,43 +1,33 @@
 With Ada.Real_Time; use Ada.Real_Time;
 with Ultrasonic; use Ultrasonic;
 With Ada.Text_IO; use Ada.Text_IO;
-with Ada.Strings.Unbounded; use Ada.Strings.Unbounded;
 
 package body TaskThink is
 
   task body think is
       myClock : Time;
       computation_time : Time_Span;
-      giveUp : Integer := 0;
    begin
       loop
          myClock := Clock;
         
          --make a decision (could be wrapped nicely in a procedure)
           if Brain.GetMeasurementSensor1 > Distance_cm(15) then            
-            MotorDriver.SetDirection (Forward); --our decision what to do based on the sensor values 
-            --Put_Line("Forward value" & Brain.GetMeasurementSensor1'Image);
-            --Put_Line("Right  value" & Brain.GetMeasurementSensor3'Image);
-            --Put_Line("Left value" & Brain.GetMeasurementSensor2'Image);
+            
+            MotorDriver.SetDirection (Forward); --our decision what to do based on the sensor values
 
-         elsif Brain.GetMeasurementSensor2 > Distance_cm(15) and giveUp < 10 then -- When distance on front is less than 15cm AND right is over 15cm
-            --Put_Line("Forward value" & Brain.GetMeasurementSensor1'Image);
-            --Put_Line("Right  value" & Brain.GetMeasurementSensor3'Image);
-             --Put_Line("Left value" & Brain.GetMeasurementSensor2'Image); 
-             delay(0.1);
-            MotorDriver.SetDirection (Left);
-            --giveUp ::= giveUp + 1;
-            
-         elsif Brain.GetMeasurementSensor3> Distance_cm(15) and giveUp < 10  then -- When distance front is less than 15cm AND left is over 15cm
-            --Put_Line("Forward value" & Brain.GetMeasurementSensor1'Image);
-            --Put_Line("Right  value" & Brain.GetMeasurementSensor3'Image);
-            --Put_Line("Left value" & Brain.GetMeasurementSensor2'Image);            
-           delay(0.1);
+         elsif Brain.GetMeasurementSensor2 > Distance_cm(15)  then 
+           
+            MotorDriver.SetDirection (Left);            
+         
+         elsif Brain.GetMeasurementSensor3> Distance_cm(15)  then
+                   
             MotorDriver.SetDirection (Right);
-            --giveUp ::= giveUp + 1;
             
-         else
+            else
+            
             MotorDriver.SetDirection (Stop);  -- If all else fails, stop car
+         
          end if;
          
          computation_time := Clock - myClock;
